62be0c9bbfa5e6b16d59e802bf18b90f20b682e3
Nelson Pereira <nelsonpereira1991@gmail.com>
2020-02-07 19:59:37 +0000

    wip

--- src/test/repository.test.ts
+++ src/test/repository.test.ts
@@ -8,9 +8,6 @@ chai.use(chaiHttp);
 
 describe('GET /api/repos/:username tests', () => {
     it("it should give a 406 error when no 'accept' header is set", (done) => {
-        expect(true).toEqual(true);
-        done();
-        /*
         chai.request(server)
             .get('/api/repos/NelsonPereira1991')
             .end((err, res) => {
@@ -19,13 +16,9 @@ describe('GET /api/repos/:username tests', () => {
                 expect(res.body.message).toEqual("Invalid value for 'accept' header, cannot produce a response matching the list of acceptable values")
                 done();
             });
-        */
     });
 
     it("it should give a 406 error when 'accept' header is set to application/xml", (done) => {
-        expect(true).toEqual(true);
-        done();
-        /*
         chai.request(server)
             .get('/api/repos/NelsonPereira1991')
             .set('accept', 'application/xml')
@@ -35,13 +28,9 @@ describe('GET /api/repos/:username tests', () => {
                 expect(res.body.message).toEqual("Invalid value for 'accept' header, cannot produce a response matching the list of acceptable values");
                 done();
             });
-        */
     });
 
     it("it should give a 404 error the user in the request is not a valid github user", (done) => {
-        expect(true).toEqual(true);
-        done();
-        /*
         chai.request(server)
             .get('/api/repos/NelsonPereira1991erororhere')
             .set('accept', 'application/json')
@@ -51,14 +40,10 @@ describe('GET /api/repos/:username tests', () => {
                 expect(res.body.message).toEqual("Could not find User");
                 done();
             });
-        */
     });
 
     //TODO NELSON the test bellow will fail when I create new repositories
     it("it should give a list of the users repositories if the user is a valid and the header is correctly set", (done) => {
-        expect(true).toEqual(true);
-        done();
-        /*
         chai.request(server)
             .get('/api/repos/NelsonPereira1991')
             .set('accept', 'application/json')
@@ -75,13 +60,9 @@ describe('GET /api/repos/:username tests', () => {
                 expect(res.body.data.repositories[0].branches[0].last_commit_sha).toEqual("b34e1c8892d71371edca38ec55dfda5c683a5889");
                 done();
             });
-        */
     });
 
     it("it should return an empty array if page=2 is passed as a param, as this user does not have enough repos for two pages", (done) => {
-        expect(true).toEqual(true);
-        done();
-        /*
         chai.request(server)
             .get('/api/repos/NelsonPereira1991?page=2')
             .set('accept', 'application/json')
@@ -90,6 +71,5 @@ describe('GET /api/repos/:username tests', () => {
                 expect(res.body.data).toBeUndefined()
                 done();
             });
-        */
     });
 });
